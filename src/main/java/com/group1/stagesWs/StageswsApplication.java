4b6442491385a77ba473cc30a3dd699c0bbbe692
Pascal Bourgoin <pascal680@hotmail.ca>
2021-09-10 15:59:53 -0400

    Premier commit pour initialiser l'environnement

---
+++ src/main/java/com/group1/stagesWs/StageswsApplication.java
@@ -0,0 +1,13 @@
+package com.group1.stagesWs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StageswsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StageswsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group1/stagesWs/StageswsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group1.stagesWs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StageswsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
