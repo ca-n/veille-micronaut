5f22ecf7ccc96038c9c6fc62c2a44d2f5e476f2b
Neil Carrie <neil.carrie@gmail.com>
2021-09-17 10:58:46 -0400

    Remove test code from main

--- src/main/java/com/group1/stagesWs/StageswsApplication.java
+++ src/main/java/com/group1/stagesWs/StageswsApplication.java
@@ -1,8 +1,7 @@
 package com.group1.stagesWs;
 
 import com.group1.stagesWs.model.Etudiant;
-import com.group1.stagesWs.service.AppService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.group1.stagesWs.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,13 +10,6 @@ public class StageswsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StageswsApplication.class, args);
-
-        AppService service = new AppService();
-
-//        Etudiant etudiant = new Etudiant("Pascal", "Bourgoin", "test@test.com", "password", "123456789", "informatique", "addresse123", "1750818",true,true);
-
-        Etudiant etudiant = new Etudiant();
-        service.addEtudiant(etudiant);
     }
 
 }
