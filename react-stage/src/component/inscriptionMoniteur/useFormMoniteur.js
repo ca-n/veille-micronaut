f2350ba49af59c8b6b0413e1f04d9809ecb00dfe
Jeremie Munger <jaymunger10@live.ca>
2021-09-22 14:40:11 -0400

    Created file for inscription moniteur in frondEnd

---
+++ react-stage/src/component/inscriptionMoniteur/useFormMoniteur.js
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import validateInfoMoniteur from "./validateInfoMoniteur";
+
+
+
+
+
+const useFormMoniteur = (callback, validateInfoEtudiant) => {
+    const url = ""
+    const [values,setValues] = useState({
+        prenom: "",
+        nom: "",
+        courriel: "",
+        password: "",
+        password2: "",
+        numTelephone: "",
+        programme: "",
+        adresse: "",
+        numMatricule: "",
+        hasLicense: "",
+        hasVoiture: "",
+    })
+    const [errors,setErrors] = useState({})
+    const [isSubmitting, setIsSubmitting] = useState(false)
+
+    const handleChange = e => {
+        const {name, value} = e.target
+        setValues({
+            ...values,
+            [name]: value,
+        })
+    }
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+
+        setErrors(validateInfoEtudiant(values))
+        setIsSubmitting(true)
+    }
+
+
+    useEffect(() => {
+        if(Object.keys(errors).length === 0 && isSubmitting) {
+            callback();
+
+            var request = new XMLHttpRequest();
+            request.open('POST', 'http://localhost:9191/stage/etudiant', true);
+            request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+
+
+            const etudiant = JSON.stringify(values);
+
+            request.send(etudiant)
+        }
+    }, [errors]
+    );
+
+
+    return {handleChange, values, handleSubmit, errors}
+};
+
+export default useFormMoniteur;
---
+++ react-stage/src/component/inscriptionMoniteur/validateInfoMoniteur.js
@@ -0,0 +1,46 @@
+export default function validateInfoMoniteur(values) {
+    let errors = {}
+
+    if(!values.prenom) {
+        errors.prenom = "Prenom requis"
+    }
+
+    if(!values.nom) {
+        errors.nom = "Nom requis"
+    }
+
+    if(!values.courriel) {
+        errors.courriel = "Courriel requis"
+    }
+
+    if(!values.password) {
+        errors.password = "Mot de passe requis"
+    } else if (!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/i.test(values.password)) {
+        errors.password = "Le mot de passe requiert au moins 6 charactères, une majuscule, une minuscule et un chiffre "
+    }
+
+    if(!values.password2){
+        errors.password2 = "Mot de passe requis"
+    } else if (values.password2 !== values.password){
+        errors.password2 = "Les mots de passe de correspondent pas"
+    }
+
+    if(!values.numTelephone) {
+        errors.numTelephone = "Numero de telephone requis"
+    }
+
+    if(!values.programme) {
+        errors.programme = "Nom du programme requis"
+    }
+
+    if(!values.adresse) {
+        errors.adresse = "Adresse requise"
+    }
+
+    if(!values.numMatricule) {
+        errors.numMatricule = "Numero de matricule requis"
+    }
+
+
+    return errors;
+}
