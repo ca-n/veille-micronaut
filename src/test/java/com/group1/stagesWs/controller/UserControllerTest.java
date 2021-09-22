a1079fe8cc8d0b0a0bc7763b1af864372eb33b71
Neil Carrie <neil.carrie@gmail.com>
2021-09-21 21:56:02 -0400

    Create testAddMoniteur() in UserControllerTests

--- src/test/java/com/group1/stagesWs/controller/UserControllerTest.java
+++ src/test/java/com/group1/stagesWs/controller/UserControllerTest.java
@@ -1,7 +1,9 @@
 package com.group1.stagesWs.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.group1.stagesWs.model.Etudiant;
+import com.group1.stagesWs.model.Moniteur;
 import com.group1.stagesWs.repositories.EtudiantRepository;
 import com.group1.stagesWs.service.UserService;
 import org.junit.jupiter.api.Test;
@@ -62,5 +64,31 @@ public class UserControllerTest {
         assertThat(etudiant).isEqualTo(actualEtudiant);
     }
 
+    @Test
+    public void testAddMoniteur() throws Exception {
+        //Arrange
+        Moniteur expected = getMoniteur();
+        when(userService.addMoniteur(expected)).thenReturn(expected);
 
+        //Act
+        MvcResult result = mockMvc.perform(post("/stage/moniteur")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(expected))).andReturn();
+
+        //Assert
+        var actualMoniteur = new ObjectMapper().readValue(result.getResponse().getContentAsString(), Moniteur.class);
+        assertThat(result.getResponse().getStatus()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(actualMoniteur).isEqualTo(expected);
+    }
+
+    private Moniteur getMoniteur() {
+        return new Moniteur(
+                "John",
+                "Doe",
+                "john.doe@example.com",
+                "pa55w0rd",
+                "000111222",
+                "Example Enterprises",
+                "123 Enterprise Lane");
+    }
 }
