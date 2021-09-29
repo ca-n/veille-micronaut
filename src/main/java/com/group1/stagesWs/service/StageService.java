61be4640485598da9136aabae27d494ab11420f4
Neil Carrie <neil.carrie@gmail.com>
2021-09-29 01:22:50 -0400

    EQ1-14-80-Create StageService Add addWhitelistToOffre Add getAllOffres Add getEtudiantOffres

--- src/main/java/com/group1/stagesWs/service/StageService.java
+++ src/main/java/com/group1/stagesWs/service/StageService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -20,10 +21,19 @@ public class StageService {
     @Autowired
     private WhitelistRepository whitelistRepository;
 
-    public Offre addWhitelistToOffre(Offre offre, Set<Etudiant> whitelisted) {
+    public Optional<Offre> addWhitelistToOffre(Offre offre, Set<Etudiant> whitelisted) {
         Whitelist whitelist = new Whitelist(whitelisted);
         whitelist = whitelistRepository.save(whitelist);
         offre.setVisibiliteEtudiant(whitelist);
-        return offreRepository.save(offre);
+        return Optional.of(offreRepository.save(offre));
+    }
+
+    public List<Offre> getAllOffres() {
+        return offreRepository.findAll();
+    }
+
+    public List<Offre> getEtudiantOffres(Etudiant etudiant) {
+        List<Whitelist> whitelists = whitelistRepository.findAllByWhitelistedEtudiant(etudiant);
+        return offreRepository.findAllByVisibiliteEtudiantIsNullOrVisibiliteEtudiantIn(whitelists);
     }
 }
