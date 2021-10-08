6c55253e7a78966a9e78af7f1e5d9bef53c09b44
Neil Carrie <neil.carrie@gmail.com>
2021-10-07 23:22:40 -0400

    EQ1-12-52-Add test Offres to db

--- src/main/java/com/group1/stagesWs/StageswsApplication.java
+++ src/main/java/com/group1/stagesWs/StageswsApplication.java
@@ -1,13 +1,33 @@
 package com.group1.stagesWs;
 
+import com.group1.stagesWs.model.Offre;
+import com.group1.stagesWs.repositories.OffreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StageswsApplication {
+public class StageswsApplication implements CommandLineRunner {
+
+    @Autowired
+    OffreRepository offreRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(StageswsApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Offre offre1 = new Offre("TITRE1", "DESCRIPTION1", "ENTREPRISE1", true);
+        Offre offre2 = new Offre("TITRE2", "DESCRIPTION2", "ENTREPRISE2", true);
+        Offre offre3 = new Offre("TITRE3", "DESCRIPTION3", "ENTREPRISE3", false);
+        Offre offre4 = new Offre("TITRE4", "DESCRIPTION4", "ENTREPRISE4", false);
+        Offre offre5 = new Offre("TITRE5", "DESCRIPTION5", "ENTREPRISE5", true);
+        offreRepository.save(offre1);
+        offreRepository.save(offre2);
+        offreRepository.save(offre3);
+        offreRepository.save(offre4);
+        offreRepository.save(offre5);
+    }
 }
