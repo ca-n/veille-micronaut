4492b5d68b81fd52d903b7fd5ba81966f86d534d
Neil Carrie <neil.carrie@gmail.com>
2021-10-16 00:26:13 -0400

    EQ1-6-84 Add CVRepository

---
+++ src/main/java/com/group1/stagesWs/repositories/CVRepository.java
@@ -0,0 +1,8 @@
+package com.group1.stagesWs.repositories;
+
+import com.group1.stagesWs.model.CV;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CVRepository extends JpaRepository<CV, Integer> {
+
+}
