c902d31600c1152746ad2dc48253bbc24f201b0f
Neil Carrie <neil.carrie@gmail.com>
2021-10-16 00:58:12 -0400

    EQ1-6-84 Add findAllByStatus to CVRepository

--- src/main/java/com/group1/stagesWs/repositories/CVRepository.java
+++ src/main/java/com/group1/stagesWs/repositories/CVRepository.java
@@ -1,8 +1,11 @@
 package com.group1.stagesWs.repositories;
 
+import com.group1.stagesWs.enums.CVStatus;
 import com.group1.stagesWs.model.CV;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CVRepository extends JpaRepository<CV, Integer> {
+import java.util.List;
 
+public interface CVRepository extends JpaRepository<CV, Integer> {
+    List<CV> findAllByStatus(CVStatus status);
 }
