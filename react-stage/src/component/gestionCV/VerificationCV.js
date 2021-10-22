2677a4c4adc27a021e886ee1772e5f7c5f1c842d
Neil Carrie <neil.carrie@gmail.com>
2021-10-22 02:07:53 -0400

    EQ1-6-85 Create frontend for cv validation

--- react-stage/src/component/gestionCV/VerificationCV.js
+++ react-stage/src/component/gestionCV/VerificationCV.js
@@ -1,20 +1,84 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams, useHistory } from 'react-router-dom'
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack'
+import CVService from '../../services/CVService.js'
+import './VerificationCV.css'
 
 const VerificationCV = () => {
-    const [cv, setCV] = useState(
-        {
-            id: String,
-            nom: String,
-            dateSoumission: String,
-            status: String,
-            data: [],
-            etudiant: {}
+    const [cv, setCV] = useState()
+    const [numPages, setNumPages] = useState(0)
+    const [page, setPage] = useState(1)
+    const [pdfScale, setPdfScale] = useState(1)
+    const { id } = useParams()
+    const history = useHistory()
+
+    useEffect(() => {
+        const getCV = async () => {
+            const cv = await CVService.getCV(id)
+            setCV(cv)
         }
+        getCV()
+    }, [id])
+
+    const onDocumentLoad = ({ numPages }) => {
+        setNumPages(numPages)
+    }
+
+    const onNextPage = () => {
+        if (page >= numPages) return
+        setPage(current => current + 1)
+    }
+
+    const onPrevPage = () => {
+        if (page <= 1) return
+        setPage(current => current - 1)
+    }
+
+    const onAccept = async () => {
+        await CVService.acceptCV(cv)
+        history.push("/gestion/cv")
+    }
+
+    const onReject = async () => {
+        await CVService.rejectCV(cv)
+        history.push("/gestion/cv")
+    }
+
+    const renderPageControls = (
+        <div className="container">
+            <div className="row center">
+                <button className="btn btn-primary col-1 prevPage" onClick={onPrevPage}>&lt;</button>
+                <div className="col-2 pages">Page {page} of {numPages}</div>
+                <button className="btn btn-primary col-1 nextPage" onClick={onNextPage}>&gt;</button>
+            </div>
+        </div>
+    )
+
+    const renderScaleControls = (
+        <div className="container">
+            <div className="row center">
+                <button className="btn btn-danger col-1 prevPage" onClick={onReject}>Rejeter</button>
+                <div className="col-1" />
+                <button className="btn btn-secondary btn-sm col-1" onClick={() => setPdfScale(scale => scale - 0.5)}>-</button>
+                <div className="col-1 pages">{pdfScale * 100}%</div>
+                <button className="btn btn-secondary btn-sm col-1" onClick={() => setPdfScale(scale => scale + 0.5)}>+</button>
+                <div className="col-1" />
+                <button className="btn btn-success col-1 nextPage" onClick={onAccept}>Accepter</button>
+            </div>
+        </div>
     )
 
     return (
-        <div>
-            
+        <div className="dark">
+            {!cv ? '' :
+                <div>
+                    {renderScaleControls}
+                    <Document file={`data:application/pdf;base64,${cv.data}`} onLoadSuccess={onDocumentLoad}>
+                        <Page pageNumber={page} scale={pdfScale} renderAnnotationLayer={false} />
+                    </Document>
+                    {numPages > 1 ? renderPageControls : ''}
+                </div>
+            }
         </div>
     )
 }
--- react-stage/src/component/gestionCV/VerificationCVList.js
+++ react-stage/src/component/gestionCV/VerificationCVList.js
@@ -11,7 +11,6 @@ const VerificationCVList = () => {
     useEffect(() => {
         const getAllCVs = async () => {
             const cvs = await CVService.getAllCVs()
-            console.log(cvs)
             setCVList(cvs);
         }
         getAllCVs();
@@ -35,14 +34,16 @@ const VerificationCVList = () => {
             <h1 className="center">Liste des CV des étudiants</h1>
             <div className="table">
                 <div className="border row container">
-                    <div className="col-5 bold auto">Nom d'étudiant</div>
-                    <div className="col-5 bold auto">Date soumission</div>
+                    <div className="col-3 bold auto">Nom d'étudiant</div>
+                    <div className="col-3 bold auto">Nom du fichier</div>
+                    <div className="col-3 bold auto">Date soumission</div>
                     <div className="col-2 bold right">Statut du CV</div>
                 </div>
                 {cvList.length === 0 ? <div className="col center">Aucun CV à afficher</div> : cvList.map(cv =>
                     <Link key={cv.id} className="row border container cvitem" to={`/gestion/cv/${cv.id}`}>
-                        <div className="col-5 auto">{cv.etudiant.nom}, {cv.etudiant.prenom}</div>
-                        <div className="col-5 auto">{cv.dateSoumission}</div>
+                        <div className="col-3 auto">{cv.etudiant.nom}, {cv.etudiant.prenom}</div>
+                        <div className="col-3 auto">{cv.nom}</div>
+                        <div className="col-3 auto">{cv.dateSoumission}</div>
                         <div className="col-2 right">{getStatusIcon(cv.status)}</div>
                     </Link>)}
             </div>
