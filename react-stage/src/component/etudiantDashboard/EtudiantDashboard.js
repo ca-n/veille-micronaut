a0937af0b6594c8ff16ec8e05eb69cf3ec47a30b
Jeremie Munger <jaymunger10@live.ca>
2021-10-29 15:46:06 -0400

    EQ1-128-135 added required useEffect() for etudiantDashboard

--- react-stage/src/component/etudiantDashboard/EtudiantDashboard.js
+++ react-stage/src/component/etudiantDashboard/EtudiantDashboard.js
@@ -2,11 +2,31 @@ import React, { useContext, useState, useEffect } from 'react'
 import { UserInfoContext } from '../../contexts/UserInfo'
 import Offres from '../Offres/Offres'
 import ContratService from '../../services/ContratService'
+import UserService from '../../services/UserService'
 import Contrat from '../contrat/Contrat'
+import CVService from '../../services/CVService.js'
 
 
 const EtudiantDashboard = () => {
     const [loggedUser, setLoggedUser] = useContext(UserInfoContext)
+    const [fullUser, setFullUser] = useState({
+        id: Number,
+        prenom: String,
+        nom: String,
+        courriel: String,
+        password: String,
+        numTelephone: String,
+        role: String,
+        programme: String,
+        adresse: String,
+        numMatricule: String,
+        hasLicense: Boolean,
+        hasVoiture: Boolean,
+        departement: String,
+        nomEntreprise: String,
+        adresseEntreprise: String,
+        specialite: String
+    })
     const [contrat, setContrat] = useState()
     const [superviseur, setSuperviseur] = useState({
         prenom: String,
@@ -16,17 +36,46 @@ const EtudiantDashboard = () => {
     const [moniteur, setMoniteur] = useState({
         prenom: String,
         nom: String,
-        courriel: String,
-        nomEntreprise: String
+        courriel: String
     })
     const [cv, setCV] = useState()
 
     useEffect(() => {
+        if (loggedUser.isLoggedIn) {
+            fetch(`http://localhost:9191/user/${loggedUser.courriel}`)
+                .then(res => {
+                    return res.json();
+                })
+                .then(data => {
+                    console.log(data, "data")
+                    setFullUser(data)
+                })
+
+        }
+    }, []);
+
+    useEffect(() => {
         const getCV = async () => {
-            const cv = await CVService.getCV(id)
+            const cv = await CVService.getCV(fullUser.id)
             setCV(cv)
         }
         getCV()
+    }, [])
+
+    useEffect(() => {
+        const getSuperviseur = async () => {
+            const superviseur = await UserService.getSuperviseur(fullUser.id)
+            setSuperviseur(superviseur)
+        }
+        getSuperviseur()
+    }, [])
+
+    useEffect(() => {
+        const getMoniteur = async () => {
+            const moniteur = await UserService.getMoniteur(fullUser.id)
+            setMoniteur(moniteur)
+        }
+        getMoniteur()
     }, [])
 
     /*
@@ -43,6 +92,32 @@ const EtudiantDashboard = () => {
     console.log(loggedUser)
     return (
         <>
+            <div>
+                <h1>Bonjour {fullUser.prenom} {fullUser.nom}</h1>
+                </div>
+            <div>
+                <h1>CV state</h1>
+            </div>
+            <div>
+                <h1>Contact</h1>
+                <table>
+                    <tr>
+                        <th>Role</th>
+                        <th>Nom</th>
+                        <th>Courriel</th>
+                    </tr>
+                    <tr>
+                        <td>Superviseur</td>
+                        <td>{superviseur.prenom} {superviseur.nom}</td>
+                        <td>{superviseur.courriel}</td>
+                    </tr>
+                    <tr>
+                        <td>Moniteur</td>
+                        <td>{moniteur.prenom} {moniteur.nom}</td>
+                        <td>{moniteur.courriel}</td>
+                    </tr>
+                </table>
+            </div>
             {contrat != null ?
                 <Contrat />
                 :
