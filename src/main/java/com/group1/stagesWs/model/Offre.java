28fa9124839bfd19867452d0a9f89b16f7081adb
Neil Carrie <neil.carrie@gmail.com>
2021-10-29 23:23:15 -0400

    EQ1-8-78 Fix constructor and add apply method to Offre

--- src/main/java/com/group1/stagesWs/model/Offre.java
+++ src/main/java/com/group1/stagesWs/model/Offre.java
@@ -53,7 +53,11 @@ public class Offre implements Serializable {
         this.horaire = horaire;
         this.nbTotalHeuresParSemaine = nbTotalHeuresParSemaine;
         this.tauxHoraire = tauxHoraire;
-        whitelist = Set.of();
-        applicants = Set.of();
+        whitelist = new HashSet<>();
+        applicants = new HashSet<>();
+    }
+
+    public void apply(Etudiant etudiant) {
+        applicants.add(etudiant);
     }
 }
