7418572a1660d64bbf564933a5d0a106b5fe7e1f
SneaKy178 <mfelton170@gmail.com>
2021-10-31 15:38:31 -0400

    EQ1-143 jai place les elements dans les coins et centre un element

--- react-stage/src/component/DashboardGestionnaire/Dashboard.js
+++ react-stage/src/component/DashboardGestionnaire/Dashboard.js
@@ -4,15 +4,25 @@ import Etudiants from "./Etudiants";
 import Moniteurs from "./Moniteurs";
 import Offres from "./Offres";
 import Superviseurs from "./Superviseurs";
-
+import "./DashboardGestionnaireCSS.css";
 const Dashboard = () => {
   return (
     <>
-      <Cvs />
-      <Etudiants />
-      <Moniteurs />
-      <Superviseurs />
-      <Offres />
+      <div className="topLeft">
+        <Etudiants />
+      </div>
+      <div className="topRight">
+        <Cvs />
+      </div>
+      <div className="center">
+        <Offres />
+      </div>
+      <div className="bottomLeft">
+        <Moniteurs />
+      </div>
+      <div className="bottomRight">
+        <Superviseurs />
+      </div>
     </>
   );
 };
