1007231604836699734ed64f8aef281b77dd1b22
Jeremie Munger <jaymunger10@live.ca>
2021-11-01 17:38:10 -0400

    EQ1-130-141 modifier model etudiant

--- src/main/java/com/group1/stagesWs/model/Etudiant.java
+++ src/main/java/com/group1/stagesWs/model/Etudiant.java
@@ -4,6 +4,7 @@ package com.group1.stagesWs.model;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import java.io.Serializable;
 import java.util.Set;
@@ -18,6 +19,9 @@ public class Etudiant extends User implements Serializable {
 
     private boolean hasLicense;
     private boolean hasVoiture;
+
+    @ManyToOne
+    private Superviseur superviseur;
 
     public Etudiant() {
         role = UserType.ETUDIANT;
