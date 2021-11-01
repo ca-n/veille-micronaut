92d2f974e5942eee5d91b670ad496e845c033944
Jeremie Munger <jaymunger10@live.ca>
2021-11-01 17:36:03 -0400

    EQ1-130-140 added missing loggedUser

--- react-stage/src/component/dashboard/Dashboard.js
+++ react-stage/src/component/dashboard/Dashboard.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import { UserInfoContext } from '../../contexts/UserInfo'
 import SuperviseurDashboard from '../superviseurDashboard/SuperviseurDashboard'
 
 
 const Dashboard = () => {
+    const [loggedUser, setLoggedUser] = useContext(UserInfoContext)
+    console.log(loggedUser)
+
     return (
         <>
             {
