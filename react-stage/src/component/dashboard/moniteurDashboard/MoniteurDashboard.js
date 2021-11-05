ed1a51fd129ee6a52e16e1802b9ee2c90cc1d46d
Jeremie Munger <jaymunger10@live.ca>
2021-11-05 10:45:54 -0400

    EQ1-129-137 updated dashboard

--- react-stage/src/component/dashboard/moniteurDashboard/MoniteurDashboard.js
+++ react-stage/src/component/dashboard/moniteurDashboard/MoniteurDashboard.js
@@ -1,70 +1,12 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { UserInfoContext } from '../../../contexts/UserInfo'
-import UserService from '../../../services/UserService'
+import React, { } from 'react'
+import Offres from '../../Offres/Offres'
 import './MoniteurDashboard.css'
 
 const MoniteurDashboard = () => {
-    const [loggedUser, setLoggedUser] = useContext(UserInfoContext)
-    const [fullUser, setFullUser] = useState({
-        id: Number,
-        prenom: String,
-        nom: String,
-        courriel: String,
-        password: String,
-        numTelephone: String,
-        role: String,
-        programme: String,
-        adresse: String,
-        numMatricule: String,
-        hasLicense: Boolean,
-        hasVoiture: Boolean,
-        departement: String,
-        nomEntreprise: String,
-        adresseEntreprise: String,
-        specialite: String
-    })
-
-    const [listEtudiants, setListEtudiants] = useState([])
-
-    useEffect(() => {
-        if (loggedUser.isLoggedIn) {
-            fetch(`http://localhost:9191/user/${loggedUser.courriel}`)
-                .then(res => {
-                    return res.json();
-                })
-                .then(data => {
-                    console.log(data, "data")
-                    setFullUser(data)
-                })
-
-        }
-    }, []);
-
-    const etudiantsList = listEtudiants.map((etudiant) =>
-        <tr key={etudiant.id.toString()}>
-            <td>{etudiant.prenom} {etudiant.nom}</td>
-            <td>{etudiant.courriel}</td>
-        </tr>);
 
     return (
         <>
-            <div>
-                <h1>Bonjour {fullUser.prenom} {fullUser.nom}</h1>
-            </div>
-            {listEtudiants.length > 0 ?
-                <div>
-                    <h1>Liste étudiants</h1>
-                    <table>
-                        <tr>
-                            <th>Nom</th>
-                            <th>Courriel</th>
-                        </tr>
-
-                        {etudiantsList}
-                    </table>
-                </div>
-                : null}
-
+            <Offres />
         </>
     )
 }
