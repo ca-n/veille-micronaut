fd4bc0d0318c688dffc56c4170d302e949f15b6f
Jeremie Munger <jaymunger10@live.ca>
2021-11-09 20:31:28 -0500

    EQ1-174-175 work on NewContrat.js

--- react-stage/src/component/contrat/newContrat/NewContrat.js
+++ react-stage/src/component/contrat/newContrat/NewContrat.js
@@ -1,15 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import useNewContrat from './useNewContrat'
 import validateInfoContrat from './validateInfoContrat';
+import OffreService from '../../../services/OffreService';
+import './NewContratCSS.css'
 
 
 const NewContrat = () => {
-    const {handleChange, values, handleSubmit, errors} = useNewContrat(validateInfoContrat);
+    const { handleChange, values, handleSubmit, errors } = useNewContrat(validateInfoContrat);
+    const [listOffres, setListOffres] = useState([])
+    const [optionOffres, setOptionOffres] = useState([])
+    const [optionEtudiants, setOptionEtudiants] = useState([])
+
+    useEffect(() => {
+        const getOffres = async () => {
+            let dbOffres
+            dbOffres = await OffreService.getAllOffres()
+
+            console.log(dbOffres, "dbOffres")
+            setListOffres(dbOffres)
+            getOptionOffres(dbOffres)
+        }
+        getOffres()
+    }, [])
+
+    const getOptionOffres = (offres) => {
+        let newOptionOffre = []
+        offres.forEach(offre => {
+            console.log(offre, "offre")
+            newOptionOffre = [...newOptionOffre, { 'id': offre.id, 'titre': offre.titre }]
+        });
+        console.log(newOptionOffre, "newOptionOffre")
+        setOptionOffres(newOptionOffre)
+    }
+
+
+    //doit recevoir l'offre lors du onChange
+    const getOptionEtudiants = (offre) => {
+        console.log("allo")
+        let listEtudiant = []
+        let applicants = offre.applicants
+        let newOptionEtudiant = []
+        applicants.forEach(etudiant => {
+            console.log(etudiant, "etudiant")
+            newOptionEtudiant = [...newOptionEtudiant, { 'id': etudiant.id, 'prenom': etudiant.prenom, 'nom': etudiant.nom }]
+        });
+        console.log(newOptionEtudiant, "newOptionEtudiant")
+        setOptionEtudiants(newOptionEtudiant)
+    }
+
+
+
 
     //apres avoir get offre
-        //->getMoniteur
-        //->list applicant
-            //->select etudiant
+    //->getMoniteur
+    //->list applicant
+    //->select etudiant
 
     return (
         <div className="form-content-right">
@@ -20,16 +65,33 @@ const NewContrat = () => {
                 <div className="form-inputs">
                     <label htmlFor="offre"
                         className="form-label">
-                        Prenom
+                        Offre
                     </label>
-                    <input id="offre" type="text" name="offre" className="form-input" placeholder="Entrez votre prenom" value={values.offre} onChange={handleChange}></input>
+                    <select>
+                        {optionOffres.map((optionOffre) => (
+                            <option onChange={getOptionEtudiants} value={optionOffre.id}>{optionOffre.titre}</option>
+                        ))}
+                    </select>
                     {errors.offre && <p>{errors.offre}</p>}
+                </div>
+                
+                <div className="form-inputs">
+                    <label htmlFor="etudiant"
+                        className="form-label">
+                        Etudiant
+                    </label>
+                    <select>
+                        {optionEtudiants.map((optionEtudiant) => (
+                            <option onChange={getOptionEtudiants} value={optionEtudiant.id}>{optionEtudiant.prenom} {optionEtudiant.nom}</option>
+                        ))}
+                    </select>
+                    {errors.etudiant && <p>{errors.etudiant}</p>}
                 </div>
 
                 <div className="form-inputs">
                     <label htmlFor="collegeEngagement"
                         className="form-label">
-                        Prenom
+                        Le Collège s’engage à :
                     </label>
                     <input id="collegeEngagement" type="text" name="collegeEngagement" className="form-input" placeholder="Entrez votre prenom" value={values.collegeEngagement} onChange={handleChange}></input>
                     {errors.collegeEngagement && <p>{errors.collegeEngagement}</p>}
@@ -38,7 +100,7 @@ const NewContrat = () => {
                 <div className="form-inputs">
                     <label htmlFor="entrepriseEngagement"
                         className="form-label">
-                        Prenom
+                        L’entreprise s’engage à :
                     </label>
                     <input id="entrepriseEngagement" type="text" name="entrepriseEngagement" className="form-input" placeholder="Entrez votre prenom" value={values.entrepriseEngagement} onChange={handleChange}></input>
                     {errors.entrepriseEngagement && <p>{errors.entrepriseEngagement}</p>}
@@ -47,7 +109,7 @@ const NewContrat = () => {
                 <div className="form-inputs">
                     <label htmlFor="etudiantEngagement"
                         className="form-label">
-                        Prenom
+                        L’étudiant s’engage à :
                     </label>
                     <input id="etudiantEngagement" type="text" name="etudiantEngagement" className="form-input" placeholder="Entrez votre prenom" value={values.etudiantEngagement} onChange={handleChange}></input>
                     {errors.etudiantEngagement && <p>{errors.etudiantEngagement}</p>}
