8ec41aa460b714e095d0eda26783e87d75c12799
Neil Carrie <neil.carrie@gmail.com>
2021-11-12 18:24:56 -0500

    Create CVService and implement saveCV

---
+++ stageman/src/main/java/xyz/carn/service/CVService.java
@@ -0,0 +1,21 @@
+package xyz.carn.service;
+
+import jakarta.inject.Singleton;
+import xyz.carn.model.CV;
+import xyz.carn.repository.CVRepository;
+
+import java.util.Optional;
+
+@Singleton
+public class CVService {
+
+    private final CVRepository cvRepository;
+
+    public CVService(CVRepository cvRepository) {
+        this.cvRepository = cvRepository;
+    }
+
+    public Optional<CV> saveCV(CV cv) {
+        return Optional.of(cvRepository.save(cv));
+    }
+}
