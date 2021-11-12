2777802c277a605dc5bb8c4c3f7687fb90d87336
Neil Carrie <neil.carrie@gmail.com>
2021-11-12 18:40:01 -0500

    Create CVServiceTest and implement testSaveCV

---
+++ stageman/src/test/java/xyz/carn/service/CVServiceTest.java
@@ -0,0 +1,42 @@
+package xyz.carn.service;
+
+import io.micronaut.test.annotation.MockBean;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+import xyz.carn.model.CV;
+import xyz.carn.repository.CVRepository;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
+
+@MicronautTest
+public class CVServiceTest {
+    @Inject
+    CVRepository cvRepository;
+
+    @Inject
+    CVService service;
+
+    @Test
+    void testSaveCV() {
+        //Arrange
+        CV expected = new CV();
+        expected.setId(1);
+        when(cvRepository.save(any(CV.class))).thenReturn(expected);
+
+        //Act
+        var returned = service.saveCV(new CV());
+
+        //Assert
+        assertThat(returned).isPresent();
+        var actual = returned.get();
+        assertThat(actual.getId()).isGreaterThan(0);
+        verify(cvRepository).save(any(CV.class));
+    }
+
+    @MockBean(CVRepository.class)
+    CVRepository cvRepository() {
+        return mock(CVRepository.class);
+    }
+}
