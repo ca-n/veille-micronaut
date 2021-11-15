e69dc956388b80dc191fd08d6f4323b9930d3862
Neil Carrie <neil.carrie@gmail.com>
2021-11-14 23:36:33 -0500

    Implement testSaveOffre in OffreServiceTest

--- stageman/src/test/java/xyz/carn/service/OffreServiceTest.java
+++ stageman/src/test/java/xyz/carn/service/OffreServiceTest.java
@@ -4,7 +4,9 @@ import io.micronaut.test.annotation.MockBean;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
+import xyz.carn.model.Etudiant;
 import xyz.carn.model.Offre;
+import xyz.carn.repository.EtudiantRepository;
 import xyz.carn.repository.OffreRepository;
 
 import java.util.List;
@@ -16,6 +18,9 @@ import static org.mockito.Mockito.*;
 public class OffreServiceTest {
     @Inject
     OffreRepository offreRepository;
+
+    @Inject
+    EtudiantRepository etudiantRepository;
 
     @Inject
     OffreService service;
@@ -32,6 +37,23 @@ public class OffreServiceTest {
         //Assert
         assertThat(returned.size()).isEqualTo(expected.size());
         verify(offreRepository).findAll();
+    }
+
+    @Test
+    void testSaveOffre() {
+        //Arrange
+        Offre expected = new Offre();
+        expected.setId(1);
+        when(offreRepository.save(any(Offre.class))).thenReturn(expected);
+
+        //Act
+        var returned = service.saveOffre(new Offre());
+
+        //Assert
+        assertThat(returned).isPresent();
+        var actual = returned.get();
+        assertThat(actual.getId()).isGreaterThan(0);
+        verify(offreRepository).save(any(Offre.class));
     }
 
     @MockBean(OffreRepository.class)
