0d4266cfb74c6ee70a081ac7cdf51efbbb41bc56
Neil Carrie <neil.carrie@gmail.com>
2021-11-14 21:27:57 -0500

    Add findAllByEtudiant to CVRepository

--- stageman/src/main/java/xyz/carn/repository/CVRepository.java
+++ stageman/src/main/java/xyz/carn/repository/CVRepository.java
@@ -3,7 +3,11 @@ package xyz.carn.repository;
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.jpa.repository.JpaRepository;
 import xyz.carn.model.CV;
+import xyz.carn.model.Etudiant;
+
+import java.util.List;
 
 @Repository
 public interface CVRepository extends JpaRepository<CV, Integer> {
+    List<CV> findAllByEtudiant(Etudiant etudiant);
 }
