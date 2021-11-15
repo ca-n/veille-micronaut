7063c4a2524d10cd9d46e30fd625575d3c870020
Neil Carrie <neil.carrie@gmail.com>
2021-11-15 01:55:48 -0500

    Implement getMoniteurOffres in OffreService

--- stageman/src/main/java/xyz/carn/service/OffreService.java
+++ stageman/src/main/java/xyz/carn/service/OffreService.java
@@ -2,8 +2,10 @@ package xyz.carn.service;
 
 import jakarta.inject.Singleton;
 import xyz.carn.model.Etudiant;
+import xyz.carn.model.Moniteur;
 import xyz.carn.model.Offre;
 import xyz.carn.repository.EtudiantRepository;
+import xyz.carn.repository.MoniteurRepository;
 import xyz.carn.repository.OffreRepository;
 
 import java.util.List;
@@ -13,8 +15,14 @@ import java.util.Optional;
 public class OffreService {
     private final OffreRepository offreRepository;
 
-    public OffreService(OffreRepository offreRepository) {
+    private final EtudiantRepository etudiantRepository;
+
+    private final MoniteurRepository moniteurRepository;
+
+    public OffreService(OffreRepository offreRepository, EtudiantRepository etudiantRepository, MoniteurRepository moniteurRepository) {
         this.offreRepository = offreRepository;
+        this.etudiantRepository = etudiantRepository;
+        this.moniteurRepository = moniteurRepository;
     }
 
     public List<Offre> getAllOffres() {
@@ -23,5 +31,18 @@ public class OffreService {
 
     public Optional<Offre> saveOffre(Offre offre) {
         return Optional.of(offreRepository.save(offre));
+    }
+
+    public List<Offre> getEtudiantOffres(String email) {
+        throw new UnsupportedOperationException("Not implemented due to issue in OffreRepository");
+//        Optional<Etudiant> etudiant = etudiantRepository.findByCourrielIgnoreCase(email);
+//        return etudiant.map(offreRepository::findAllByWhitelistContainsAndValidTrue)
+//                .orElse(List.of());
+    }
+
+    public List<Offre> getMoniteurOffres(String email) {
+        Optional<Moniteur> moniteur = moniteurRepository.findByCourrielIgnoreCase(email);
+        return moniteur.map(offreRepository::findAllByMoniteur)
+                .orElse(List.of());
     }
 }
