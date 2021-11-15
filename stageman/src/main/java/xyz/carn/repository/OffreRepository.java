0959869656ca9134b077091aba2563e98a0d8dba
Neil Carrie <neil.carrie@gmail.com>
2021-11-15 01:55:02 -0500

    Add findAllByMoniteur to OffreRepository

--- stageman/src/main/java/xyz/carn/repository/OffreRepository.java
+++ stageman/src/main/java/xyz/carn/repository/OffreRepository.java
@@ -1,9 +1,17 @@
 package xyz.carn.repository;
 
+import io.micronaut.data.annotation.Query;
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.jpa.repository.JpaRepository;
+import xyz.carn.model.Etudiant;
+import xyz.carn.model.Gestionnaire;
+import xyz.carn.model.Moniteur;
 import xyz.carn.model.Offre;
+
+import java.util.List;
 
 @Repository
 public interface OffreRepository extends JpaRepository<Offre, Integer> {
+//    List<Offre> findAllByWhitelistContainsAndValidTrue(Etudiant etudiant);
+    List<Offre> findAllByMoniteur(Moniteur moniteur);
 }
