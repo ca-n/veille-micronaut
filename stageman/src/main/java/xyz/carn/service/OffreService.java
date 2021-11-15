349cef3ed1ffbbc0bc9e353db66679e18d255553
Neil Carrie <neil.carrie@gmail.com>
2021-11-14 23:37:26 -0500

    Implement saveOffre in OffreService

--- stageman/src/main/java/xyz/carn/service/OffreService.java
+++ stageman/src/main/java/xyz/carn/service/OffreService.java
@@ -1,10 +1,13 @@
 package xyz.carn.service;
 
 import jakarta.inject.Singleton;
+import xyz.carn.model.Etudiant;
 import xyz.carn.model.Offre;
+import xyz.carn.repository.EtudiantRepository;
 import xyz.carn.repository.OffreRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Singleton
 public class OffreService {
@@ -16,5 +19,9 @@ public class OffreService {
 
     public List<Offre> getAllOffres() {
         return offreRepository.findAll();
+    }
+
+    public Optional<Offre> saveOffre(Offre offre) {
+        return Optional.of(offreRepository.save(offre));
     }
 }
