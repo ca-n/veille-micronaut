6af51d79b5aa4095f73d644ef6c54b4783df0221
Neil Carrie <neil.carrie@gmail.com>
2021-11-14 21:56:39 -0500

    Implement getCV in CVController

--- stageman/src/main/java/xyz/carn/controller/CVController.java
+++ stageman/src/main/java/xyz/carn/controller/CVController.java
@@ -2,9 +2,7 @@ package xyz.carn.controller;
 
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
-import io.micronaut.http.annotation.Body;
-import io.micronaut.http.annotation.Controller;
-import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.*;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
 import xyz.carn.model.CV;
@@ -24,5 +22,12 @@ public class CVController {
         return service.saveCV(cv)
                 .map(HttpResponse::ok)
                 .orElse(HttpResponse.status(HttpStatus.CONFLICT));
+    }
+
+    @Get("/{id}")
+    public HttpResponse<CV> getCV(@PathVariable int id) {
+        return service.getCV(id)
+                .map(HttpResponse::ok)
+                .orElse(HttpResponse.status(HttpStatus.NOT_FOUND));
     }
 }
