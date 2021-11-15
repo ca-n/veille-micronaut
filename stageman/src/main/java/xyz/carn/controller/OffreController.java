77931f40ebb560a9958b930bfd83145ac5d532ba
Neil Carrie <neil.carrie@gmail.com>
2021-11-15 02:34:32 -0500

    Implement getMoniteurOffres in OffreController

--- stageman/src/main/java/xyz/carn/controller/OffreController.java
+++ stageman/src/main/java/xyz/carn/controller/OffreController.java
@@ -4,6 +4,7 @@ import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
@@ -31,5 +32,16 @@ public class OffreController {
         return service.saveOffre(offre)
                 .map(HttpResponse::ok)
                 .orElse(HttpResponse.status(HttpStatus.CONFLICT));
+    }
+
+    @Get("/etudiant/{email}")
+    public HttpResponse<List<Offre>> getEtudiantOffres(@PathVariable String email) {
+        throw new UnsupportedOperationException("Not implemented due to issue in OffreRepository");
+//        return HttpResponse.ok(service.getEtudiantOffres(email));
+    }
+
+    @Get("/moniteur/{email}")
+    public HttpResponse<List<Offre>> getMoniteurOffres(@PathVariable String email) {
+        return HttpResponse.ok(service.getMoniteurOffres(email));
     }
 }
