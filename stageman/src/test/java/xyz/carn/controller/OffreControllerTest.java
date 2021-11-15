8ed9da13f7cf21bc9f56de7855ab9296059bd699
Neil Carrie <neil.carrie@gmail.com>
2021-11-15 02:42:47 -0500

    Implement testApplyForOffre in OffreControllerTest

--- stageman/src/test/java/xyz/carn/controller/OffreControllerTest.java
+++ stageman/src/test/java/xyz/carn/controller/OffreControllerTest.java
@@ -8,11 +8,13 @@ import io.micronaut.test.annotation.MockBean;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
+import xyz.carn.model.Etudiant;
 import xyz.carn.model.Offre;
 import xyz.carn.service.OffreService;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -79,6 +81,23 @@ public class OffreControllerTest {
         assertThat(response.status()).isEqualTo(HttpStatus.OK);
         assertThat(response.body().size()).isEqualTo(expected.size());
         verify(service).getMoniteurOffres(anyString());
+    }
+
+    @Test
+    void testApplyForOffre() {
+        //Arrange
+        Offre expected = new Offre();
+        expected.setApplicants(Set.of(new Etudiant()));
+        when(service.applyForOffre(anyInt(), anyString())).thenReturn(Optional.of(expected));
+        var request = HttpRequest.POST("/1/apply", "etudiant@example.com");
+
+        //Act
+        var response = client.toBlocking().exchange(request, Offre.class);
+
+        //Assert
+        assertThat(response.status()).isEqualTo(HttpStatus.OK);
+        assertThat(response.body().getApplicants().size()).isGreaterThan(0);
+        verify(service).applyForOffre(anyInt(), anyString());
     }
 
     @MockBean(OffreService.class)
