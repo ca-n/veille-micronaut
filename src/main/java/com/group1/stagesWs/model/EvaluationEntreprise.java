109db4bac7f8dc7f514125925c081e6b3ba0b059
Neil Carrie <neil.carrie@gmail.com>
2021-11-18 03:29:08 -0500

    EQ1-17-73 Create EvaluationEntreprise entity

---
+++ src/main/java/com/group1/stagesWs/model/EvaluationEntreprise.java
@@ -0,0 +1,36 @@
+package com.group1.stagesWs.model;
+
+import com.group1.stagesWs.SessionManager;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Data
+@Entity
+public class EvaluationEntreprise implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int numeroStage;
+    private char[] evaluation;
+    private String commentaires;
+    private int stagePrefere;
+    private int nombreStagiaires;
+    private boolean garderStagiaire;
+    private boolean variableShifts;
+    private LocalDate dateCreation;
+    private String session;
+
+    @OneToOne
+    private Contrat contrat;
+
+    @ManyToOne
+    private Superviseur superviseur;
+
+    public EvaluationEntreprise() {
+        dateCreation = LocalDate.now();
+        session = SessionManager.CURRENT_SESSION.getNomSession();
+    }
+}
