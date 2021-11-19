2b0ba6991f6a06eccee5b5fb8bc5bfbc271e7f66
Neil Carrie <neil.carrie@gmail.com>
2021-11-18 20:15:04 -0500

    EQ1-18-71 Add saveContrat, getMoniteurContrats and getSuperviseurContrats to ContratService.js

--- react-stage/src/services/ContratService.js
+++ react-stage/src/services/ContratService.js
@@ -1,11 +1,30 @@
-const url = 'http://localhost:9191/stage/contrat/'
+const url = 'http://localhost:9191/contrat'
 
 const ContratService = {
-    getContrat: async (email) => {
-        //const res = await fetch(url + email)
-        //const data = await res.json()
-        //return data
+    saveContrat: async (contrat) => {
+        const res = await fetch(url, 
+        {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(contrat)
+        })
+        const data = await res.json()
+        return data
     },
+
+    getMoniteurContrats: async (moniteurCourriel) => {
+        const res = await fetch(url + '/moniteur/courriel/' + moniteurCourriel)
+        const data = await res.json()
+        return data
+    },
+
+    getSuperviseurContrats: async (superviseurCourriel) => {
+        const res = await fetch(url + '/superviseur/courriel/' + superviseurCourriel)
+        const data = await res.json()
+        return data
+    }
 }
 
 export default ContratService
