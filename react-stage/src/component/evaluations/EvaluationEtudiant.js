6e94dfa96dd2d234ca71d8e83eebade6876de57a
Neil Carrie <neil.carrie@gmail.com>
2021-11-19 04:18:48 -0500

    EQ1-18-71 Finish EvaluationEtudiant frontend

--- react-stage/src/component/evaluations/EvaluationEtudiant.js
+++ react-stage/src/component/evaluations/EvaluationEtudiant.js
@@ -1,6 +1,9 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useHistory } from 'react-router'
+import Swal from 'sweetalert2'
 import { UserInfoContext } from '../../contexts/UserInfo'
+import ContratService from '../../services/ContratService'
+import EvaluationService from '../../services/EvaluationService'
 import FormEvaluationEtudiant from './FormEvaluationEtudiant'
 import ListEtudiantToEvaluate from './ListEtudiantToEvaluate'
 
@@ -11,13 +14,10 @@ const EvaluationEtudiant = () => {
     const [currentContrat, setCurrentContrat] = useState()
 
     useEffect(() => {
-        //if (!loggedUser.isLoggedIn || loggedUser.role !== "MONITEUR") history.push("/login")
+        if (!loggedUser.isLoggedIn || loggedUser.role !== "MONITEUR") history.push("/login")
 
         const getContrats = async () => {
-            //const contratList = await ContratService.getMoniteurContratsToEvaluate(loggedUser.courriel)
-            const res = await fetch("http://localhost:9191/contrat")
-            const contratList = await res.json()
-            //
+            const contratList = await ContratService.getMoniteurContratsToEvaluate(loggedUser.courriel)
             console.log(contratList)
             setContrats(contratList)
         }
@@ -30,9 +30,30 @@ const EvaluationEtudiant = () => {
         setCurrentContrat(contrats.find(contrat => contrat.id === id))
     }
 
+    const handleCancel = () => {
+        setCurrentContrat(undefined)
+    }
+
+    const submitEvaluation = async (evaluation) => {
+        var result = await EvaluationService.saveEvaluationEtudiant(evaluation)
+        if (!result.error) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Votre évaluation a été envoyée.',
+                showConfirmButton: false,
+                timer: 1500
+            })
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Erreur, veuillez réessayer plus tard.'
+            })
+        }
+    }
+
     return (
         <div>
-            {currentContrat === undefined ? <ListEtudiantToEvaluate contrats={contrats} onClick={selectContrat}/> : <FormEvaluationEtudiant contrat={currentContrat} />}
+            {currentContrat === undefined ? <ListEtudiantToEvaluate contrats={contrats} onClick={selectContrat}/> : <FormEvaluationEtudiant contrat={currentContrat} onClickSubmit={submitEvaluation} onClickCancel={handleCancel}/>}
         </div>
     )
 }
